f26bc6bacfd81c57e30dbf79c2586728d3dfb9c8
Ruslan Prohorchik <prohorchikrv@gmail.com>
2019-06-10 16:08:41 +0300

    Add project files.

---
+++ FirstConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello first console Application!!!");
+
+
+            Console.ReadKey();
+        }
+    }
+}
