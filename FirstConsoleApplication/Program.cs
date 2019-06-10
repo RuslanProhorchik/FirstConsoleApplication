dc8d9b62ae0663940320b266ff40dc3aa2cfd4c3
Ruslan Prohorchik <prohorchikrv@gmail.com>
2019-06-10 16:11:41 +0300

    Add a new method - UseSampleProject to display new console message

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -4,10 +4,16 @@ namespace FirstConsoleApplication
 {
     class Program
     {
+        public static void UseSampleProject()
+        {
+            Console.WriteLine("Hello UseSampleProject!!!");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello first console Application!!!");
 
+            UseSampleProject();
 
             Console.ReadKey();
         }
