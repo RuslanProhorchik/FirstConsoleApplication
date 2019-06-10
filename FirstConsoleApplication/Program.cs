3ec046e3d81efb19043b127ca13ef5a64f5ab6ff
RuslanProhorchik <33485996+RuslanProhorchik@users.noreply.github.com>
2019-06-10 16:24:07 +0300

    Add new changes 22

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -14,12 +14,20 @@ namespace FirstConsoleApplication
             Console.WriteLine("Hello UseSampleProjectUpdateFromGit!!!");
         }
 
+        public static void UseSampleProjectUpdateFromGit22()
+        {
+            Console.WriteLine("Hello UseSampleProjectUpdateFromGit22!!!");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello first console Application!!!");
 
             UseSampleProject();
+            
             UseSampleProjectUpdateFromGit();
+            
+            UseSampleProjectUpdateFromGit22();
 
             Console.ReadKey();
         }
