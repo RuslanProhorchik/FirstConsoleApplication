0f644756fa39480c260e8d105edce3d5f3f30d7f
RuslanProhorchik <33485996+RuslanProhorchik@users.noreply.github.com>
2019-06-10 16:17:35 +0300

    Add new method - updated from Git

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -9,11 +9,17 @@ namespace FirstConsoleApplication
             Console.WriteLine("Hello UseSampleProject!!!");
         }
 
+        public static void UseSampleProjectUpdateFromGit()
+        {
+            Console.WriteLine("Hello UseSampleProjectUpdateFromGit!!!");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello first console Application!!!");
 
             UseSampleProject();
+            UseSampleProjectUpdateFromGit();
 
             Console.ReadKey();
         }
